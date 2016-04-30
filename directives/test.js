e999dc12528caa07b45eca563ba0ad645b956919
Kardo <kardoj@gmail.com>
2016-04-30 10:58:03 +0300

    Initial commit

---
+++ directives/test.js
@@ -0,0 +1,9 @@
+/**
+ * Created by kardo on 30.04.2016.
+ */
+
+var helloWorld = app.directive('helloWorld', function() {
+    return {
+        template: 'Hello World!'
+    };
+});
---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+/**
+ * Created by kardo on 30.04.2016.
+ */
+
+var app = angular.module("todoApp", []);
