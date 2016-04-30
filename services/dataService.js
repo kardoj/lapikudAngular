737a8d7a35c0eb381742b4cd94d18fcd564f131e
Kardo <kardoj@gmail.com>
2016-04-30 14:55:13 +0300

    Task 10  - emulated rest service

--- services/dataService.js
+++ services/dataService.js
@@ -1,3 +1,10 @@
 /**
  * Created by kardo on 30.04.2016.
  */
+
+app.service("dataService", function($http){
+    this.getTodos = function(callback){
+        $http.get("REST/todos.json").
+            then(callback);
+    }
+});
