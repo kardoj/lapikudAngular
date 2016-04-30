055022eb9a24c12004ea5b3a7bd144d810b0d966
Kardo <kardoj@gmail.com>
2016-04-30 15:15:25 +0300

    Task 11 - Delete

--- services/dataService.js
+++ services/dataService.js
@@ -3,8 +3,13 @@
  */
 
 app.service("dataService", function($http){
+    
     this.getTodos = function(callback){
         $http.get("REST/todos.json").
             then(callback);
-    }
+    };
+    
+    this.deleteTodo = function(todo){
+        console.log("Deleted from db " + JSON.stringify(todo));
+    };
 });
